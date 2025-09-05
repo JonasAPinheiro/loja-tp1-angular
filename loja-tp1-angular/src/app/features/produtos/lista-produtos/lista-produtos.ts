748b376c56abfe5afbce2f68810103b049abf7ab
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2025-09-05 20:37:05 -0300

    Implentando filtro por promoção

--- loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Produto } from '../../../model/produto';
 import { CardProduto } from "../card-produto/card-produto";
 
@@ -23,16 +23,36 @@ export class ListaProdutos {
       nome: "Produto 2",
       descricao: "Desc. Prod 2",
       preco: 179.90,
+      estado: "novo"
     },
     {
       id: 3,
       nome: "Produto 3",
       descricao: "Desc. Prod 3",
       preco: 179.90,
+      estado: "esgotado"
+    },
+    {
+      id: 4,
+      nome: "Produto 4",
+      descricao: "Desc. Prod 4",
+      preco: 279.90
     }
   ]
 
+  apenasPromo = signal(false);
+
+  prodExibidos = computed(() => this.apenasPromo() ? this.produtos.filter(p => p.promo) : this.produtos);
+
+  alternarPromo(){
+    this.apenasPromo.update(p => !p);
+  }
+
   onAddProduct(produto: {id: number, quantity: number}){
     alert(`Produto ${produto.id}\n${produto.quantity} unidades`);
+  }
+
+  onViewProduct(id: number){
+    alert(`Id do produto: ${id}`);
   }
 }
