9d65ee639a581e6d5794becb4820a29f66aaf77e
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2025-09-12 20:06:09 -0300

    Criação do logger.service

--- loja-tp1-angular/src/app/app.ts
+++ loja-tp1-angular/src/app/app.ts
@@ -1,15 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './core/header/header'
 import { Footer } from "./core/footer/footer";
-import { Banner } from "./core/banner/banner";
 import { QuantidadeControle } from "./shared/quantidade-controle/quantidade-controle";
-import { Produto } from './model/produto';
 import { CardProduto } from "./features/produtos/card-produto/card-produto";
 import { ListaProdutos } from "./features/produtos/lista-produtos/lista-produtos";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Banner, ListaProdutos],
+  imports: [Header, Footer, ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -12,31 +12,34 @@ export class ListaProdutos {
   produtos: Array<Produto> = [
     {
       id: 1,
-      nome: "Produto 1",
+      nome: "Pelucia Pikachu",
       descricao: "Desc. Prod 1",
       preco: 179.90,
-      imageURL: 'images/logoifsp.png',
+      imageURL: 'https://entertainmentcenternl.com/cdn/shop/products/Pikachu-Plush.jpg?v=1616780856&width=713',
       promo: true
     },
     {
       id: 2,
-      nome: "Produto 2",
+      nome: "Pelucia Mega Charizard X",
       descricao: "Desc. Prod 2",
       preco: 179.90,
+      imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ5fAA5POrHMKtq0JWfO-5Pmq7lHODEx73-A&s",
       estado: "novo"
     },
     {
       id: 3,
-      nome: "Produto 3",
+      nome: "Pelucia Mewtwo",
       descricao: "Desc. Prod 3",
       preco: 179.90,
+      imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv9Go84D0PdkfsHr817Zq7LbfyEvSsPiqY3g&s",
       estado: "esgotado",
     },
     {
       id: 4,
-      nome: "Produto 4",
+      nome: "Pelucia Ash Greninja",
       descricao: "Desc. Prod 4",
       preco: 279.90,
+      imageURL: "https://entertainmentcenternl.com/cdn/shop/files/greninja.png?v=1716389591&width=713",
       estado: "novo",
     }
   ]
